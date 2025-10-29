87d380469526a49c07e2494f10af3408deef5c09
null <null>
2025-10-29 14:39:56 +0530

    Solution

--- src/app/components/grid-example/grid-example.ts
+++ src/app/components/grid-example/grid-example.ts
@@ -1,5 +1,5 @@
-import { Component, computed, inject } from '@angular/core';
-import { GridModule } from "@syncfusion/ej2-angular-grids";
+import { Component, ViewChild, computed, inject } from '@angular/core';
+import { GridComponent, GridModule } from "@syncfusion/ej2-angular-grids";
 import { InvoiceLineColumns } from "./invoice-line-columns";
 import { SalesInvoiceDomainService } from "./sales-invoice.domain-service";
 import { RegieSalesInvoice, TermijnSalesInvoice, WorkOrderSalesInvoice } from "./salesinvoices";
@@ -13,6 +13,7 @@ import { RegieSalesInvoice, TermijnSalesInvoice, WorkOrderSalesInvoice } from ".
   styleUrl: './grid-example.css',
 })
 export class GridExample {
+  @ViewChild('grid') grid?: GridComponent;
   salesInvoiceDomainService = inject(SalesInvoiceDomainService);
   invoiceLineColumns = new InvoiceLineColumns();
 
@@ -35,4 +36,17 @@ export class GridExample {
   protected readonly RegieSalesInvoice = RegieSalesInvoice;
   protected readonly TermijnSalesInvoice = TermijnSalesInvoice;
   protected readonly WorkOrderSalesInvoice = WorkOrderSalesInvoice;
+
+  buttonClick(str: string) {
+    if (str === "Term") {
+      this.salesInvoiceDomainService.salesInvoice.set(TermijnSalesInvoice);
+    } else if (str === "Regie") {
+      this.salesInvoiceDomainService.salesInvoice.set(RegieSalesInvoice);
+    } else if (str === "Work Order") {
+      this.salesInvoiceDomainService.salesInvoice.set(WorkOrderSalesInvoice);
+    }
+
+    setTimeout(()=> this.grid?.refreshColumns(), 50) 
+  }
 }
+ 
